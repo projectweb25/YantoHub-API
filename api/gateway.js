e6fc365f30aa37516d59d98bcf228c8f7a00abbb
projectweb25 <putraboccorr@gmail.com>
2026-05-19 01:01:29 +0800

    Update gateway.js

--- api/gateway.js
+++ api/gateway.js
@@ -1,5 +1,8 @@
 module.exports = function(req) {
   const ua = req.headers['user-agent'] || '';
   const acc = req.headers['accept'] || '';
-  return !acc.includes('text/html') || ua.includes('Roblox') || ua.includes('Xeno') || ua.includes('Delta') || ua === "";
+  if (ua.includes('Roblox') || ua.includes('Xeno') || ua.includes('Delta') || ua === "") {
+    return true;
+  }
+  return !acc.includes('text/html');
 };
