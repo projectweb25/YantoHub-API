fd931456f72b01828f036b833b53d7634316eccd
projectweb25 <putraboccorr@gmail.com>
2026-05-19 00:26:02 +0800

    Update and rename index.js to scripts.js

---
+++ api/scripts.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "vip-yanto": "https://raw.githubusercontent.com/Yantohub25/sc/main/V",
+  "free-yanto": "https://raw.githubusercontent.com/Yantohub25/sc/main/C",
+  "script-baru": "https://raw.githubusercontent.com/Yantohub25/sc/main/fitur_baru",
+  "test-xeno": "https://raw.githubusercontent.com/Yantohub25/sc/main/test"
+};
