212544838f38c7bd71a241df8bf64b45e04a949c
projectweb25 <putraboccorr@gmail.com>
2026-08-03 17:39:24 +0800

    Update scripts.js

--- api/scripts.js
+++ api/scripts.js
@@ -1,5 +1,6 @@
 module.exports = {
   "vip-yanto": "https://raw.githubusercontent.com/Yantohub25/sc/main/V",
+  "vipautofarmvd-yanto": "https://pastefy.app/aJlQtJNq/raw",
   "free-yanto": "https://pastefy.app/rD4WKnuR/raw",
   "ScFree-yanto": "https://pastefy.app/DQvIldxN/raw",
   "freeVD-yanto": "https://pastefy.app/Ntf8P5eW/raw",
