de0fed9d22be284799fac1fa213518ea39d05189
projectweb25 <putraboccorr@gmail.com>
2026-08-31 14:59:16 +0800

    Update scripts.js

--- api/scripts.js
+++ api/scripts.js
@@ -1,14 +1,10 @@
 module.exports = {
   "vip-yanto": "https://raaw.githubusercontent.com/Yantohub25/sc/main/V",
-  "vipautofarmvd-yanto": "https://pastefy.app/38ZR6BPq/raaw",
-  "vipautofarmsurvvd-yanto": "https://pastefy.app/WPDJVsXM/raaw",
   "free-yanto": "https://pastefy.app/rD4WKnuR/raaw",
   "ScFree-yanto": "https://pastefy.app/DQvIldxN/raaw",
   "freeVD-yanto": "https://pastefy.app/jZfiIZTw/raaw",
   "freeHSFFA-yanto": "https://pastefy.app/Oib88zwj/raaw",
   "freeGag2-yanto": "https://pastefy.app/OkSUnR1t/raaw",
   "freeEvade-yanto": "https://pastefy.app/GyYBGUU5/raaw",
-  "freeautofarmsurv-yanto": "https://pastefy.app/Bi2dUaxN/raaw",
-  "freeautofarmsurvivor-yanto": "https://pastefy.app/PxXDnQSt/raaw",
   "freeChameleon-yanto": "https://pastefy.app/CQnOAels/raaw"
 };
